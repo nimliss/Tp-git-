52734bb3b6635a3d00596873beac2d283483449d
Antoine <antoine958.ad@gmail.com>
2019-02-08 11:31:01 +0100

    add var to script

--- src/application.bash
+++ src/application.bash
@@ -1,4 +1,6 @@
- ### Application.bash - 201x - DESHAIES Antoine ###
-echo "Copyright Company - 201x"
-echo "Build : 10:00"
-echo "version 1.1"
+### Application.bash - 201x - Deshaies Antoine ###
+version = "2"
+build_hour = "11H00"
+
+echo "Build ${build_hour}"
+echo "version ${version}"
